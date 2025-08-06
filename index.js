fe4cde64d9a7c43b415cb3033dc9fc71a4be602e
Mahadeva B M <mahadeva@accelnomics.com>
2025-08-06 11:55:40 +0530

    Adding the post and get method

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 app.get('/',(req,res)=>{
-    res.send("Hi you are accessing the simple http server");
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/submit',(req,res)=>{
+    const {name , email} = req.body;
+    res.send(`Thank you for you response ${name} with you mail ${email}`);
 });
 
 
